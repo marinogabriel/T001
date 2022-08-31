83af18836b2c82a380560f06fb4d9beed33c65fa
marinogabriel <112117885+marinogabriel@users.noreply.github.com>
2022-08-31 15:06:00 -0300

    Update ClienteDAO.java

--- src/main/java/model/ClienteDAO.java
+++ src/main/java/model/ClienteDAO.java
@@ -1,10 +1,45 @@
-package model;
+package model
 
-public class ClienteDAO {
+public class ClienteDAO implements DAO<Cliente> {
     
-    public void RetrieveAll()
-    {
-        
+    private List<Cliente> clientes = new ArrayList<>();
+    
+    public ClienteDAO() {
+        clientes.add(new Cliente(1, "Rua X", "19912345678", "12345678", "pedrinho@gmail.com"));
+        clientes.add(new Cliente(2, "Rua Y", "1134781234", "87654321", "joaozinho@dac.unicamp.br"));
     }
-            
+    
+    @Override
+    public Optional<Cliente> retrieve(int id) {
+        return Optional.ofNullable(clientes.retrieve((int) id));
+    }
+    
+    @Override
+    public List<Cliente> retrieveAll() {
+        return clientes;
+    }
+    
+    @Override
+    public void create(Cliente cliente) {
+        clientes.add(cliente);
+    }
+    
+    @Override
+    public void update(Cliente cliente, String[] params) {
+        cliente.setEndereco(Objects.requireNonNull(
+          params[0], "Endereco nao pode ser null"));
+        cliente.setTelefone(Objects.requireNonNull(
+          params[1], "Telefone nao pode ser null"));
+	    cliente.setCep(Objects.requireNonNull(
+          params[2], "Cep nao pode ser null"));
+        cliente.setEmail(Objects.requireNonNull(
+          params[3], "Email nao pode ser null"));
+
+        clientes.add(cliente);
+    }
+    
+    @Override
+    public void delete(Cliente cliente) {
+        clientes.remove(cliente);
+    }
 }
